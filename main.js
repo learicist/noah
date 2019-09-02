879578a1dc22cbf81ecda1c063b3116791ace3db
The Doctor <learicist@gmail.com>
2019-09-02 14:59:31 -0400

    first commit with JS

---
+++ main.js
@@ -0,0 +1,12 @@
+$(document).ready(function () {
+	
+	console.log('Happy Birthday Loser!');
+	
+	$('#smile').click(function () {
+		alert("IT'S MY BIRTHDAYYYYY!!!");
+		console.log('Just kidding, loooovvveee yyyoooouu');
+	});
+	
+	
+	
+});
